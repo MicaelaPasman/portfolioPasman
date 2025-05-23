18fb785b2dda4254ea4c8c8fdc1fab52e37c607d
Micaela Pasman <micapasman@gmail.com>
2025-05-22 21:47:26 -0300

    actua,lizacion1

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.getElementById('hamburger');
     const menu = document.getElementById('menu');
+    const links = menu.querySelectorAll('a');
 
     hamburger.addEventListener('click', () => {
         menu.classList.toggle('show');
     });
-});
 
+    // Cierra el menú cuando se hace clic en un enlace
+    links.forEach(link => {
+        link.addEventListener('click', () => {
+            menu.classList.remove('show');
+        });
+    });
+});
